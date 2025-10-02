28e19e1dc4cc26afef2853c079aa22e14350dc45
carlosafg5598 <carlosfdezvivas98@gmail.com>
2025-10-02 11:52:14 +0200

    primer comentario

---
+++ introASP/introASP/Models/Brand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace introASP.Models;
+
+public partial class Brand
+{
+    public int IdBrand { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public virtual ICollection<Cerveza> Cervezas { get; set; } = new List<Cerveza>();
+}
